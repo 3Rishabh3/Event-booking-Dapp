6cda20ea8c321047470ae9f00c7dabfb6974adde
Ankur Vadhwani <www.nankur113@gmail.com>
2021-08-23 20:04:35 +0530

    changed

--- src/components/swap/Swap.jsx
+++ src/components/swap/Swap.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./Swap.css";
 import Button from "@material-ui/core/Button";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 export default function Swap() {
   window.document.title = "Swap";
   let [fromAmt, setfromAmt] = useState(0);
+  let [toAmt, settoAmt] = useState(0);
   return (
     <div className="root">
       <div id="swapcontainer">
@@ -11,13 +13,46 @@ export default function Swap() {
         <div className="childcontainer" id="From">
           <div className="selectfromcurrent">
             FROM
-            <Button className="modalbtn" variant="contained" color="primary">
+            <Button
+              className="modalbtn"
+              endIcon={<ExpandMoreIcon fontSize="large" />}
+              variant="contained"
+              color="primary"
+            >
               ETH
             </Button>
           </div>
-          <input id="enterfromamount" value={fromAmt} type="text" />
+          <input
+            id="enterfromamount"
+            onChange={(e) => {
+              setfromAmt(e.target.value);
+            }}
+            value={fromAmt}
+            type="text"
+          />
         </div>
-        <div className="childcontainer" id="To"></div>
+        <div className="childcontainer" id="To">
+          <div className="selecttocurrent">
+            TO
+            <Button
+              className="modalbtn"
+              variant="contained"
+              color="secondary"
+              style={{ color: "white", background: "black" }}
+              disabled
+            >
+              OurC
+            </Button>
+          </div>
+          <input
+            id="entertoamount"
+            onChange={(e) => {
+              settoAmt(e.target.value);
+            }}
+            value={toAmt}
+            type="text"
+          />
+        </div>
       </div>
     </div>
   );
