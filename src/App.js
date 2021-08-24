515a8ea75406520d809651ba1e90f588d9a6901b
Rishabh Gupta <3rishabh3@gmail.com>
2021-08-24 17:45:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,49 +1,151 @@
 import { Route, Switch, useLocation } from "react-router-dom";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import { useState, useEffect } from "react";
+
 // all pages imported
-import Homepage from "./components/homepage/Homepage";
-import Createevent from "./components/createevent/Createevent";
-import Dashboard from "./components/dashboard/Dashboard";
-import Pastevent from "./components/pastevent/Pastevent";
-import Swap from "./components/swap/Swap";
-import Upcomingevent from "./components/upcomingevent/Upcomingevent";
-import Userticket from "./components/userticket/Userticket";
-import ConnectToMetmask from "./MetamaskConnect";
-import { useState } from "react";
+import NavBar from "./components/NavBar";
+import Homepage from "./components/Homepage";
+import Swap from "./components/Swap";
+import Userticket from "./components/Userticket";
+
+//Contexts
+import UserContext from "./Contexts/UserContext";
+
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import { toast } from "react-toastify";
+
+//Web3 library
+import Web3 from "web3";
+
+//import Abis
+import SwapAbi from "./contracts/Swap.json";
+import TokenAbi from "./contracts/Token.json";
 
 export default function App() {
   let location = useLocation();
-  let [currentaccount, setCurrentAccountToHome] = useState(undefined);
-  console.log(currentaccount);
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [networkId, setNetWorkId] = useState(null);
+  const [token, setToken] = useState(null);
+  const [swap, setSwap] = useState(null);
+  const [tokenBalance, setTokenBalance] = useState(null);
+  const [ethBalance, setEthBalance] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    loadWeb3();
+  }, []);
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      await window.ethereum.enable();
+      loadBlockchainData();
+    } else {
+      toast("Please Connect To A Wallet. Try Using Metamask.", {
+        type: "error",
+        autoClose: 15000,
+      });
+    }
+  };
+
+  const loadBlockchainData = async () => {
+    if (typeof window.ethereum === "undefined") {
+      return;
+    }
+
+    const web3 = new Web3(window.ethereum);
+
+    var accounts = await web3.eth.getAccounts();
+    setCurrentAccount(accounts[0]);
+
+    const networkId = await web3.eth.net.getId();
+    setNetWorkId(networkId);
+
+    if (networkId !== 4) {
+      toast("Use Rinkeby Test network", { type: "error", autoClose: 15000 });
+      return;
+    }
+
+    var ethBalance = await web3.eth.getBalance(accounts[0]);
+    setEthBalance(ethBalance);
+    console.log(ethBalance);
+
+    // Load Token
+    const tokenData = TokenAbi.networks[networkId];
+    if (tokenData) {
+      const token = new web3.eth.Contract(TokenAbi.abi, tokenData.address);
+      setToken(token);
+      let tokenBalance = await token.methods.balanceOf(accounts[0]).call();
+      setTokenBalance(tokenBalance.toString());
+    } else {
+      toast("Token contract not deployed to detected network.", {
+        type: "error",
+      });
+    }
+
+    // Load Swap
+    const SwapData = SwapAbi.networks[networkId];
+    if (SwapData) {
+      const swap = new web3.eth.Contract(SwapAbi.abi, SwapData.address);
+      setSwap(swap);
+    } else {
+      toast("Swap contract not deployed to detected network.", {
+        type: "error",
+      });
+    }
+  };
+
+  const buyTokens = (etherAmount) => {
+    setIsLoading(true);
+    swap.methods
+      .buyTokens()
+      .send({ value: etherAmount, from: currentAccount })
+      .on("transactionHash", (hash) => {
+        setIsLoading(false);
+      });
+  };
+
+  const sellTokens = (tokenAmount) => {
+    setIsLoading(true);
+    token.methods
+      .approve(swap.address, tokenAmount)
+      .send({ from: currentAccount })
+      .on("transactionHash", (hash) => {
+        swap.methods
+          .sellTokens(tokenAmount)
+          .send({ from: currentAccount })
+          .on("transactionHash", (hash) => {
+            setIsLoading(false);
+          });
+      });
+  };
+
+  if (networkId !== 4)
+    return (
+      <h1 className="text-center mt-5">Please connect to Rinkeby Testnet...</h1>
+    );
 
   return (
     <>
-      <ConnectToMetmask setCurrentAccountToHome={setCurrentAccountToHome} />
-      {currentaccount === undefined ? (
-        <CircularProgress />
-      ) : (
-        <h3
-          style={{
-            margin: "0",
-            padding: "8px 4px",
-            background: "white",
-            boxShadow: "0px 2px 5px #0007",
-            textAlign: "center"
-          }}
-        >
-          Current Address : {currentaccount}
-        </h3>
-      )}
-
-      <Switch location={location} key={location.key}>
-        <Route path="/" exact component={Homepage} />
-        <Route path="/upcomingevent" exact component={Upcomingevent} />
-        <Route path="/pastevent" exact component={Pastevent} />
-        <Route path="/createevent" exact component={Createevent} />
-        <Route path="/dashboard" exact component={Dashboard} />
-        <Route path="/swap" exact component={Swap} />
-        <Route path="/userticket" exact component={Userticket} />
-      </Switch>
+      <ToastContainer />
+      <UserContext.Provider
+        value={{
+          currentAccount,
+          ethBalance,
+          tokenBalance,
+          token,
+          swap,
+          buyTokens,
+          sellTokens,
+        }}
+      >
+        <NavBar address={currentAccount} />
+        <Switch location={location} key={location.key}>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/swap" exact component={Swap} />
+          <Route path="/userticket" exact component={Userticket} />
+        </Switch>
+      </UserContext.Provider>
     </>
   );
 }
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,162 @@
+import React from "react";
+import { Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Link } from "react-router-dom";
+// icons
+import EventAvailableIcon from "@material-ui/icons/EventAvailable";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import EventIcon from "@material-ui/icons/Event";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import SwapHorizIcon from "@material-ui/icons/SwapHoriz";
+import ConfirmationNumberIcon from "@material-ui/icons/ConfirmationNumber";
+import EnhancedEncryptionIcon from "@material-ui/icons/EnhancedEncryption";
+export default function Home() {
+  const classes = useStyles();
+  window.document.title = "homepage";
+  return (
+    <div class={classes.homepage}>
+      <h1 class={classes.heading}>
+        {" "}
+        <i>
+          <EnhancedEncryptionIcon color="blue" fontSize={"large"} />
+        </i>{" "}
+        Ticky Ticket
+      </h1>
+      <h1 class={classes.subHeading}>What are you going to do?</h1>
+      <div class={classes.root}>
+        <h1 class={classes.subHeading}>Welcome to Ticky Ticket</h1>
+        <p class={classes.ptag}>
+          The PhoenixDAO Events Marketplace is a dApp that allows people to
+          create events and sell tickets online, with the option to make an
+          event, paid or free.
+        </p>
+        <p class={classes.ptag}>
+          The tickets created on this service are ERC721 tokens, which means
+          that users are able to move, gift, or sell those tickets to other
+          users.
+        </p>
+        <p class={classes.ptag}>
+          The PhoenixDAO Events Marketplace is a dApp powered by the Matic
+          Network. In order to create events or purchase tickets, you are
+          required have an wallet. If you do not have one currently, you can use
+          MetaMask.
+        </p>
+
+        <Grid container>
+          <Grid item xs={12} sm={6} md={4}>
+            <Link style={{ textDecoration: "none" }} to="/upcomingevent">
+              {" "}
+              <Paper class={classes.paper}>
+                <EventIcon style={{ fontSize: "4rem" }} />
+                <br />
+                Upcomming Events
+              </Paper>
+            </Link>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Link style={{ textDecoration: "none" }} to="/pastevent">
+              <Paper class={classes.paper}>
+                <EventAvailableIcon style={{ fontSize: "4rem" }} />
+                <br />
+                Past Events
+              </Paper>
+            </Link>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Link style={{ textDecoration: "none" }} to="/createevent">
+              <Paper class={classes.paper}>
+                <AddBoxIcon style={{ fontSize: "4rem" }} />
+                <br />
+                Create Event
+              </Paper>
+            </Link>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Link style={{ textDecoration: "none" }} to="/dashboard">
+              <Paper class={classes.paper}>
+                <DashboardIcon style={{ fontSize: "4rem" }} />
+                <br />
+                Dashboard
+              </Paper>
+            </Link>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Link style={{ textDecoration: "none" }} to="/swap">
+              <Paper class={classes.paper}>
+                <SwapHorizIcon style={{ fontSize: "4rem" }} />
+                <br />
+                Swap Ticket
+              </Paper>
+            </Link>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4}>
+            <Link style={{ textDecoration: "none" }} to="/userticket">
+              <Paper class={classes.paper}>
+                <ConfirmationNumberIcon style={{ fontSize: "4rem" }} />
+                <br />
+                Your Ticket
+              </Paper>
+            </Link>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundPosition: "center",
+    padding: "24px",
+    borderRadius: "5px"
+  },
+  homepage: {
+    width: "80%",
+    margin: "auto",
+    marginTop: "24px",
+    marginBottom: "24px"
+  },
+  heading: {
+    color: "white",
+    fontSize: "3rem",
+    fontWeight: "900",
+    margin: 0,
+    padding: 0,
+    marginTop: "10%"
+  },
+  subHeading: {
+    color: "white",
+    fontSize: "1.8rem",
+    padding: "0",
+    margin: "0",
+    marginBottom: "2rem"
+  },
+  ptag: {
+    color: "white",
+    textAlign: "justify"
+  },
+  paper: {
+    padding: theme.spacing(2),
+    height: "120px",
+    margin: "5%",
+    textAlign: "center",
+    color: "white",
+    border: "2px solid white",
+    borderRadius: "10px",
+    transition: ".2s steps(60)",
+    "&:hover": {
+      transform: "scale(1.03)",
+      transformOrigin: "center",
+      border: "2px solid #0f09",
+      cursor: "pointer",
+      background: "#8a86"
+    }
+  }
+}));
